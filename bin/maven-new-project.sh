b31dfc44e9895bec2d76cf1c92565bac8e4e8028
Frank Maritato <frank.maritato@openx.com>
2012-09-10 11:35:23 -0700

    updated some info and formatting

--- bin/maven-new-project.sh
+++ bin/maven-new-project.sh
@@ -2,14 +2,19 @@
 
 GROUP=
 ARTIFACT=
-VERSION=1.0-SNAPSHOT
-ARCH_VERSION=1.0.3
+VERSION=1.0.0.0-SNAPSHOT
+ARCH_VERSION=1.1.1
 while getopts v:g:a: x
 do
   case $x in 
-    g) GROUP=$OPTARG;;
-    a) ARTIFACT=$OPTARG;;
-    v) VERSION=$OPTARG;;
+    g) GROUP=$OPTARG
+      ;;
+    a) ARTIFACT=$OPTARG
+      ;;
+    v) VERSION=$OPTARG
+      ;;
+    r) ARCH_VERSION=$OPTARG
+      ;;
   esac
 done
 
@@ -18,11 +23,12 @@ if [ -z "$GROUP" -o -z "$ARTIFACT" ]; then
   exit 1
 fi
 
-mvn archetype:create \
--DremoteRepositories=http://maven.corp.atti.com:9999/nexus/content/groups/public \
--DarchetypeGroupId=com.yellowpages.maven \
--DarchetypeArtifactId=yp-archetype \
--DarchetypeVersion=$ARCH_VERSION \
--DgroupId=$GROUP \
--Dversion=$VERSION \
--DartifactId=$ARTIFACT
+mvn archetype:generate                         \
+  -DarchetypeRepository=http://maven.openx.org:8081/artifactory/simple/libs-release-local \
+  -DarchetypeGroupId=org.openx.maven           \
+  -DarchetypeArtifactId=openx-maven-archetype  \
+  -DarchetypeVersion=$ARCH_VERSION             \
+  -DinteractiveMode=false                      \
+  -DgroupId=$GROUP                             \
+  -DartifactId=$ARTIFACT                       \
+  -Dversion=$VERSION
